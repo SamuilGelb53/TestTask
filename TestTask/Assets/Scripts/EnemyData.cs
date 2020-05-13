7df5c22cbc1c95e46dc2a1d08af94f7cf13cbf3b
SamuilGelb53 <samuilgelb53@gmail.com>
2020-05-13 14:21:44 +0300

    Release v2

---
+++ TestTask/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemies/Standart Enemy", fileName = "New Enemy")]
+public class EnemyData : ScriptableObject
+{
+    [Tooltip("Колличество здоровья")]
+    [SerializeField] private float _healthAmount;
+    public float _HealthAmount
+    {
+        get { return _healthAmount; }
+        set { _healthAmount = value; }
+    }
+
+    [Tooltip("Колличество здоровья")]
+    [Range(1f, 10f)]
+    [SerializeField] private float _movingSpeed = 2f;
+    public float _MovingSpeed
+    {
+        get { return _movingSpeed; }
+        set { _movingSpeed = value; }
+    }
+
+    [Tooltip("Колличество здоровья")]
+    [SerializeField] private float _damage;
+    public float _Damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+
+    [Tooltip("Модель врага")]
+    [SerializeField] private GameObject _object;
+    public GameObject _Object
+    {
+        get { return _object; }
+        set { _object = value; }
+    }
+
+    [Tooltip("Цена смерти")]
+    [SerializeField] private int _cost;
+    public int _Cost
+    {
+        get { return _cost; }
+        set { _cost = value; }
+    }
+
+
+}
---
+++ TestTask/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerData : MonoBehaviour
+{
+    [Tooltip("Колличество денег")]
+    [SerializeField] private int _Coins;
+    public int _coins
+    {
+        get { return _Coins; }
+        set
+        {
+            _Coins = value;
+            //При любом изменении значений будет обновлятся текст
+            UpdateText();
+        }
+    }
+    [Tooltip("Количество жизней")]
+    [SerializeField] private float _Lives = 15;
+    public float _lives
+    {
+        get { return _Lives; }
+        set
+        {
+            _Lives = value;
+            //При любом изменении значений будет обновлятся текст, если жизни закончились - перезапустить уровень
+            if (_Lives <= 0)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            UpdateText();
+        }
+    }
+    [Tooltip("Текст меш для данных пользователя")]
+    [SerializeField] private TextMeshProUGUI _playerDataText;
+
+    public void Awake()
+    {
+        _coins = _Coins;
+        _lives = _Lives;
+    }
+
+    //Обновление текста
+    public void UpdateText()
+    {
+        _playerDataText.text = "Lives: " + (int)_lives + "\nCoins: " + (int)_coins;
+    }
+}
